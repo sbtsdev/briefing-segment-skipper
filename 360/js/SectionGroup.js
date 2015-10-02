824389af4834444ce9ba53c45d0dc2af6b4a300f
Joshua Cottrell <joshua.e.cottrell@gmail.com>
2015-10-01 23:43:14 -0400

    Fixed play and pause responding to main 360 player clicks

--- 360/js/SectionGroup.js
+++ 360/js/SectionGroup.js
@@ -2,10 +2,10 @@
 (function (win, doc) {
     'use strict';
 SBTS.maker('SBTS.am.SectionGroup');
-SBTS.am.SectionGroup = function (briefingId, sectionId) {
-    this.briefing = doc.querySelector(briefingId);
+SBTS.am.SectionGroup = function (briefingQuery, sectionQuery) {
+    this.briefing = doc.querySelector(briefingQuery);
     this.els = Array.prototype.slice.call(
-            doc.querySelectorAll(sectionId));
+            doc.querySelectorAll(sectionQuery));
     this._sectionsPlaying = false;
     this.sections = [];
     this.els.forEach(this.addSections.bind(this));
@@ -21,6 +21,14 @@ SBTS.am.SectionGroup.prototype.playing = function (time) {
         if (full[index].playing(time)) {
             me._sectionsPlaying = true;
         }
+    });
+};
+SBTS.am.SectionGroup.prototype.paused = function (time) {
+    var me = this;
+    time = parseInt(time, 10);
+    me._sectionsPlaying = false;
+    me.sections.forEach(function (current, index, full) {
+        full[index].paused(time);
     });
 };
 SBTS.am.SectionGroup.prototype.finished = function () {
